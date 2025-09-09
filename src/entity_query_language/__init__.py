4715fb4c68a5d6705757ade4667f400094579b84
AbdelrhmanBassiouny <abassiou@uni-bremen.de>
2025-09-10 00:13:29 +0200

    [EQLPredicate] cached predicates.

--- src/entity_query_language/__init__.py
+++ src/entity_query_language/__init__.py
@@ -7,7 +7,7 @@ logger.setLevel(logging.INFO)
 
 from .entity import (entity, a, an, let, the, set_of,
                      and_, or_, not_, contains, in_)
-from .predicate import predicate, symbol
+from .predicate import predicate, symbol, Predicate
 from .rule import refinement, alternative, symbolic_mode
 from .conclusion import Add, Set
 from .failures import MultipleSolutionFound, NoSolutionFound
